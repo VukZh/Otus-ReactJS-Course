26252e725a867d928e8f8f5a579721c9f8b583cc
VukZh <vukfx@inbox.ru>
2022-12-19 23:07:01 +0600

    add mathematical operations and tests

---
+++ src/fct.test.ts
@@ -0,0 +1,5 @@
+import { factorial } from './fct'
+
+test('factorial 5 to equal 120', () => {
+    expect(factorial(5)).toBe(120);
+});
---
+++ src/mul.test.ts
@@ -0,0 +1,5 @@
+import { mul } from './mul'
+
+test('multiplication 10 * 2 to equal 20', () => {
+    expect(mul(10, 2)).toBe(20);
+});
