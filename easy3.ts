444d7b0ef06e95bff5fb779deb1c4246b70dcd0e
VukZh <vukfx@inbox.ru>
2023-01-25 22:40:33 +0600

    resolve easy3

--- easy3.ts
+++ easy3.ts
@@ -3,7 +3,9 @@
 // Нужно заменить FIXME на соответствующий тип
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+type Obj = Record<any, any>;
+type Key = keyof Obj;
+type FIXME = Record<Key, Obj[Key]>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const omit = <T extends Record<any, any>, K extends keyof T>(
@@ -13,4 +15,25 @@ export const omit = <T extends Record<any, any>, K extends keyof T>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { [keyToOmit]: _, ...withoutKey } = obj;
   return withoutKey;
-};
+}
+
+// test
+
+const test1 = omit({
+  a: 1,
+  b: 2,
+  c: 3
+}, 'a');
+
+console.log('> ', test1);
+
+const test2 = omit({
+  123: 456,
+  abc: 'def',
+  obj: {
+    1: 2,
+    a: 'b'
+}
+}, 'abc');
+
+console.log('> ', test2);
