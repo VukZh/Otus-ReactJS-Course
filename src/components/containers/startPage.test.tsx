30ace6c7f96cb482d3034d187adba65cee89a829
VukZh <vukfx@inbox.ru>
2023-03-09 00:35:02 +0600

    tests refactoring

--- src/components/containers/startPage.test.tsx
+++ src/components/containers/startPage.test.tsx
@@ -5,12 +5,17 @@
 import '@testing-library/react';
 import '@testing-library/jest-dom';
 import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { StartPage } from './StartPage';
 
 describe('StartPage test', () => {
   test('StartPage test', () => {
-    render(<StartPage></StartPage>);
+    render(
+      <BrowserRouter>
+        <StartPage></StartPage>
+      </BrowserRouter>
+    );
     expect(screen.getByText(/Cryptocurrency checker app/i)).toBeInTheDocument();
     expect(screen.getByText(/Input your name/i)).toBeInTheDocument();
     expect(screen.getAllByRole('button')).toHaveLength(1);
--- src/components/controls/controlButtons.test.tsx
+++ src/components/controls/controlButtons.test.tsx
@@ -5,23 +5,27 @@
 import '@testing-library/react';
 import '@testing-library/jest-dom';
 import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import { render, screen } from '@testing-library/react';
 import { ControlButtons } from './ControlButtons';
 
 test('Button test', () => {
   render(
-    <ControlButtons
-      showModal={() => null}
-      historicity={true}
-      history={[]}
-      changeCurrency={() => null}
-      listSize={3}
-      changeRandomCurrency={() => null}
-    ></ControlButtons>
+    <BrowserRouter>
+      <ControlButtons
+        showModal={() => null}
+        historicity={true}
+        history={[]}
+        changeCurrency={() => null}
+        listSize={3}
+        changeRandomCurrency={() => null}
+      ></ControlButtons>
+    </BrowserRouter>
   );
   const text = screen.getAllByText(/Please/i);
   expect(text[0]).toBeInTheDocument();
   const buttons = screen.getAllByRole('button');
   expect(buttons[4]).toBeInTheDocument();
-  expect(buttons).toHaveLength(7);
+  expect(buttons).toHaveLength(8);
 });
