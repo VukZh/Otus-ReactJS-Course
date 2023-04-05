1921e75c3e26a97ed1121b4abedd7df6c0cff9fc
VukZh <vukfx@inbox.ru>
2023-04-05 11:34:27 +0600

    add redux-saga

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,12 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { reducer } from './reducer';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+const sagaMiddleware = createSagaMiddleware();
+export const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, sagaMiddleware))
+);
 export type TypedDispatch = typeof store.dispatch;
