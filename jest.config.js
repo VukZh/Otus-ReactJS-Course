3476fc550777bbdc693353e002297808e0eb7b7f
VukZh <vukfx@inbox.ru>
2023-04-12 14:26:24 +0600

    add 404 page and change Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,20 @@
-module.exports = {
-  moduleNameMapper: {
-    '\\.(png|css)$': '<rootDir>/path-to-fileMock.js',
-  },
-  testEnvironment: 'jsdom',
-  preset: 'ts-jest',
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: [],
+};
+
+module.exports = async function () {
+  const makeConfig = await createJestConfig(customJestConfig);
+  const finalJestConfig = await makeConfig();
+
+  finalJestConfig.transformIgnorePatterns[0] =
+    '/node_modules/(?!@react-dnd|react-dnd|dnd-core|react-dnd-html5-backend/)';
+
+  return finalJestConfig;
 };
