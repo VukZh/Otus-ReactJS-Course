73345c05847d6753e37525d6160f2584d83b18ef
VukZh <vukfx@inbox.ru>
2023-04-21 14:59:14 +0600

    add startPage tests

--- src/components/containers/startPage.test.tsx
+++ src/components/containers/startPage.test.tsx
@@ -6,7 +6,7 @@ import '@testing-library/react';
 import '@testing-library/jest-dom';
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { StartPage } from './StartPage';
 
 describe('StartPage test', () => {
@@ -20,5 +20,34 @@ describe('StartPage test', () => {
     expect(screen.getByText(/Input your name/i)).toBeInTheDocument();
     expect(screen.getAllByRole('button')).toHaveLength(1);
     expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+  test('Input change test', () => {
+    render(
+      <BrowserRouter>
+        <StartPage></StartPage>
+      </BrowserRouter>
+    );
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    fireEvent.change(input, {target: {value: 'vvv'}});
+    expect(input.value).toBe('vvv');
+  });
+  test('Submit test', () => {
+    Object.defineProperty(window, 'localStorage', {
+      value: {
+        getItem: jest.fn(() => null),
+        setItem: jest.fn(() => null),
+      },
+      writable: true,
+    });
+    const { container } = render(
+      <BrowserRouter>
+        <StartPage></StartPage>
+      </BrowserRouter>
+    );
+    const form = container.querySelector('form');
+    expect(window.localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(window.localStorage.setItem).toHaveBeenCalledTimes(0);
+    fireEvent.submit(form);
+    expect(window.localStorage.setItem).toHaveBeenCalledTimes(1);
   });
 });
