2864aa7beee44caee9ad6a67be478abad06367fe
VukZh <vukfx@inbox.ru>
2023-04-23 23:12:51 +0600

    rangeSlider refactoring

--- src/components/rangeSlider.tsx
+++ src/components/rangeSlider.tsx
@@ -1,16 +1,24 @@
 import {FC, useState} from 'react';
 import InputRange from 'react-input-range';
 
+type RangeValueType = {
+  min: number,
+  max: number
+}
 const RangeSlider: FC = () => {
-  const [value, setValue] = useState({min: 5, max: 55});
+  const [value, setValue] = useState<RangeValueType>({min: 5, max: 55});
   return (
     <>
       <InputRange
         draggableTrack
         maxValue={100}
         minValue={0}
-        onChange={(value) => setValue(value)}
-        onChangeComplete={(value) => console.log(JSON.stringify(value))}
+        onChange={(value) => setValue(value as RangeValueType)}
+        onChangeComplete={(value) => {
+          if ("min" in value && value.min < 0) {value.min = 0}
+          if ("max" in value && value.max > 100) {value.max = 100}
+          console.log(JSON.stringify(value));
+        }}
         value={ value }
       />
     </>
